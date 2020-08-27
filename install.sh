1c4c1a6a7a1b2106739e71255f3936318e39c9ce
Dana Basken <dbasken@gmail.com>
2020-08-27 18:28:56 +0000

    first commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+NODE=`which node`
+
+npm install
+
+cat cloud-minder-central.service.template | sed s+__USER__+$USER+ | sed s+__PWD__+$PWD+ | sed s+__NODE__+$NODE+ > cloud-minder-central.service
+
+sudo cp cloud-minder-central.service /etc/systemd/system/
+sudo systemctl --system daemon-reload
+sudo systemctl enable cloud-minder-central
+sudo systemctl start cloud-minder-central
+
